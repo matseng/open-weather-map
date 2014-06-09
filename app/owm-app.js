fb8b6885626515a9c88be76dce29d8af798e67af
Michael Tseng <matseng@gmail.com>
2014-06-09 16:53:15 -0700

    Injected  to access variables in URL name

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,12 +1,21 @@
 angular.module('OWMApp', ['ngRoute']);
 
 angular.module('OWMApp', ['ngRoute'])
-  .config(function($routeProvider){
-      $routeProvider.when('/', {
-          templateUrl : './home.html',
-          controller : 'HomeCtrl'
-      });
-  })
-  .controller('HomeCtrl', function($scope) {
-      //empty for now
-  });;
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl : './home.html',
+            controller : 'HomeCtrl'
+        }).when('/cities/:city', {
+            templateUrl : './city.html',
+            controller : 'CityCtrl'
+        });
+    })
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CityCtrl', function($scope, $routeParams) {
+        $scope.city = $routeParams.city;
+    });
+
+
+  // $locationProvider.html5Mode(true);
