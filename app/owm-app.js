d61f24ad18f50c0a21b19a859364a8f79bf74313
Michael Tseng <matseng@gmail.com>
2014-06-09 17:21:31 -0700

    Added 'resolve' attribute to router - acts as middleware to test URLs before templates & controllers are rendered

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,20 +1,37 @@
 angular.module('OWMApp', ['ngRoute']);
 
 angular.module('OWMApp', ['ngRoute'])
+    .value('owmCities',
+      ['New York', 'Dallas', 'Chicago'])
     .config(function($routeProvider) {
-        $routeProvider.when('/', {
+        $routeProvider
+        .when('/', {
             templateUrl : './home.html',
             controller : 'HomeCtrl'
-        }).when('/cities/:city', {
+        })
+        .when('/cities/:city', {
             templateUrl : './city.html',
-            controller : 'CityCtrl'
+            controller : 'CityCtrl',
+            resolve : {
+              resolvedCity: function(owmCities, $route, $location) {
+              var city = $route.current.params.city;
+              if(owmCities.indexOf(city) == -1 ) {
+                  $location.path('/error');
+                  return;
+              }
+              return city;
+            }
+          }
+        })
+        .when('/error', {
+          template : '<p>Error Page Not Found</p>'
         });
     })
     .controller('HomeCtrl', function($scope) {
         //empty for now
     })
-    .controller('CityCtrl', function($scope, $routeParams) {
-        $scope.city = $routeParams.city;
+    .controller('CityCtrl', function($scope, resolvedCity) {
+        $scope.city = resolvedCity;
     });
 
 
